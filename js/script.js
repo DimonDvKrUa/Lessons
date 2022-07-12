5156b38d410650b8703752fe2e1ab22df15b6e71
Dimon Home <dimon@dv.kr.ua>
2022-07-12 22:34:48 +0300

    Упражнение по написанию кода 3

--- js/script.js
+++ js/script.js
@@ -1,44 +1,41 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
-const numberOfFilms = + prompt ('Сколько фильмов вы уже посмотрели?', '');
+for (let i=5; i<11; i++) {
+   console.log(i);
+}
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+for (let i=20; i>9; i--) {
+   if (i === 13) {break;}
+   console.log(i);
+}
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 2; i < 11; i++) {
+   if (i % 2 === 0) {
+       console.log(i);
+   }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 2; i <= 16; i++) {
+   if (i % 2 === 0) {
+      continue;
+   } else {
+      console.log(i);
+   }
+}
 
-console.log(personalMovieDB);
+let i=2;
+while (i <= 16) {
+   if (i % 2 === 0) {
+      i++;
+      continue;
+   } else {
+      console.log(i);
+      i++;
+   }
+}
+
+let arr = [];
+for (let i = 5; i < 11; i++) {
+   arr.push(i);
+}
+console.log(arr);
