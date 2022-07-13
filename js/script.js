3be87ae41fab7a960b7363e47778f092c67e3f86
Dimon Home <dimon@dv.kr.ua>
2022-07-13 22:06:10 +0300

    Упражнение по написанию кода 4 и 5

--- js/script.js
+++ js/script.js
@@ -1,41 +1,42 @@
 'use strict';
 
-for (let i=5; i<11; i++) {
-   console.log(i);
-}
 
-for (let i=20; i>9; i--) {
-   if (i === 13) {break;}
-   console.log(i);
-}
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+// for (let i = 0; i < arr.length; i++) {
+//    result.push(arr[i]);
+// }
+// console.log(result);
 
-for (let i = 2; i < 11; i++) {
-   if (i % 2 === 0) {
-       console.log(i);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// for (let i = 0; i < data.length; i++) {
+//    if (typeof(data[i]) == "number") {
+//       data[i] = data[i]*2;
+//    } else if (typeof(data[i]) == "string") {
+//       data[i] += " - done";
+//    } else{
+//       continue;
+//    }
+// }
+// console.log(data);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
+// for (let i = data.length-1; i >= 0; i--) {
+//    result.push(data[i]);
+// }
+// console.log(result);
+
+const lines = 5;
+let result = '';
+for (let i = 0; i <= lines; i++) {
+   for (let j = 1; j <= lines-i; j++) {
+      result += " ";
    }
-}
-
-for (let i = 2; i <= 16; i++) {
-   if (i % 2 === 0) {
-      continue;
-   } else {
-      console.log(i);
+   for (let k = 1; k <= 1+2*i; k++) {
+      result += "*";
    }
+   result += "\n";
 }
-
-let i=2;
-while (i <= 16) {
-   if (i % 2 === 0) {
-      i++;
-      continue;
-   } else {
-      console.log(i);
-      i++;
-   }
-}
-
-let arr = [];
-for (let i = 5; i < 11; i++) {
-   arr.push(i);
-}
-console.log(arr);
+console.log(result);
