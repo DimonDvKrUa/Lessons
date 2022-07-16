d803ccd917d836427a4f80018d23f466a158985f
Dimon Home <dimon@dv.kr.ua>
2022-07-16 20:31:33 +0300

    Упражнение по написанию кода 6

--- js/script.js
+++ js/script.js
@@ -1,42 +1,32 @@
 'use strict';
 
-
-// const arr = [3, 5, 8, 16, 20, 23, 50];
-// const result = [];
-// for (let i = 0; i < arr.length; i++) {
-//    result.push(arr[i]);
-// }
-// console.log(result);
-
-
-// const data = [5, 10, 'Shopping', 20, 'Homework'];
-// for (let i = 0; i < data.length; i++) {
-//    if (typeof(data[i]) == "number") {
-//       data[i] = data[i]*2;
-//    } else if (typeof(data[i]) == "string") {
-//       data[i] += " - done";
-//    } else{
-//       continue;
-//    }
-// }
-// console.log(data);
-
-// const data = [5, 10, 'Shopping', 20, 'Homework'];
-// const result = [];
-// for (let i = data.length-1; i >= 0; i--) {
-//    result.push(data[i]);
-// }
-// console.log(result);
-
-const lines = 5;
-let result = '';
-for (let i = 0; i <= lines; i++) {
-   for (let j = 1; j <= lines-i; j++) {
-      result += " ";
-   }
-   for (let k = 1; k <= 1+2*i; k++) {
-      result += "*";
-   }
-   result += "\n";
+function sayHello(name) {
+   return 'Привет, '+ name;
 }
-console.log(result);
+
+console.log(sayHello('Антон'));
+
+function returnNeighboringNumbers(num) {
+   const arr = [];
+   arr.push(num-1);
+   arr.push(num);
+   arr.push(num+1);
+   return arr;
+}
+
+console.log(returnNeighboringNumbers(33));
+
+function getMathResult(num, count) {
+   let res = '';
+   if (typeof(count) != 'number' || count < 1) {
+      res = num;
+   } else {
+      for (let i = 1; i < count; i++) {
+         res += num*i + '---';
+      }
+      res += num*count;
+   }
+   return res;
+}
+
+console.log(getMathResult(20, -5));
