b0ed478fdadab15ef840535f0641722fabaf780f
Dimon Home <dimon@dv.kr.ua>
2022-07-19 01:03:21 +0300

    Упражнение по написанию кода 8, 9

--- js/script.js
+++ js/script.js
@@ -41,3 +41,66 @@ console.log(getCoupeNumber(0));
 console.log(getCoupeNumber(7.7));
 console.log(getCoupeNumber(-10));
 
+// Место для первой задачи
+function getTimeFromMinutes(minuts) {
+    let hour = 0,
+        min = 0,
+        txt = 'час';
+    if (typeof(minuts) == 'number' && minuts >= 0 && Number.isInteger(minuts)) {
+        hour = Math.floor(minuts/60);
+        min = minuts-(hour*60);
+        if (hour > 1 && hour <5) {
+            txt += 'а';
+        } else if(hour > 4 || hour == 0) { 
+            txt += 'ов';
+        }
+        return (`Это ${hour} ${txt} и ${min} минут`);
+    } else {
+        return ('Ошибка, проверьте данные');
+    }
+}
+console.log(getTimeFromMinutes(60));
+console.log(getTimeFromMinutes(150));
+console.log(getTimeFromMinutes(50));
+console.log(getTimeFromMinutes(0));
+console.log(getTimeFromMinutes(-150));
+console.log(getTimeFromMinutes(350));
+
+// Место для второй задачи
+function findMaxNumber(x1, x2, x3, x4) {
+    if (typeof(x1, x2, x3, x4) == 'number') {
+        return Math.max(x1, x2, x3, x4);
+    } else {
+        return(0);
+    }
+    
+}
+
+console.log(findMaxNumber(1, 5, 6.6, 11));
+console.log(findMaxNumber(1, 5, '6', '10'));
+
+
+function fib(x) {
+    let ch1 = 0,
+        ch2 = 1,
+        ch3 = 0,
+        str = '';
+    if (x === 1) {str = '0';} 
+    else if (x === 2) {str = '0 1';}
+    else {
+        str = '0 1';
+        for (let i = 2; i < x; i++) {
+            ch3 = ch1 + ch2;
+            ch1 = ch2;
+            ch2 = ch3;
+            str += ' ' + ch3;
+        }
+    }
+    return(str);
+}
+
+console.log(fib(4));
+console.log(fib(7));
+console.log(fib('7'));
+console.log(fib(1));
+console.log(fib(0));
