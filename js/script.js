1308cdd5b262d2bfcda9df7aed8f2f9fff6ea6a9
Dimon Home <dimon@dv.kr.ua>
2022-07-20 22:19:01 +0300

    Практика , ч4

--- js/script.js
+++ js/script.js
@@ -1,53 +1,78 @@
+/* Задание на урок:
+
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
 'use strict';
 
-const family = ['Peter', 'Ann', 'Alex', 'Linda'];
-function showFamily(arr) {
-    if (arr.length > 0) {
-        return `Семья состоит из: ${arr.join(' ')}`;
-    } else {
-        return `Семья пуста`;
-    }
-}
-console.log(showFamily([family]));
 
-
-const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
-function standardizeStrings(arr) {
-    for (let key in arr) {
-        console.log(arr[key].toLowerCase());
-    }
-}
-standardizeStrings(favoriteCities);
-
-const someString = 'This is some strange string';
-
-function reverse(str) {
-    if (typeof(str) === 'string') {
-        let arr = [...str].reverse();
-        return arr.join('');
-    } else {
-        return 'Ошибка!';
-    }
-}
-
-console.log(reverse(someString));
-
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
-//console.log(baseCurrencies.concat(additionalCurrencies));
-
-function availableCurr(arr, missingCurr) {
-    if (arr.length > 0) {
-        let str = 'Доступные валюты:\n';
-        for (let key in arr) {
-            if (arr[key] !== missingCurr) {
-                str += `${arr[key]}\n`;
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+     
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFils: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                  b = prompt('На сколько оцените его?', '');
+    
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
             }
         }
-        return str;
-    } else {
-        return ('нет доступных валют');
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    }, 
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    }, 
+    writeYourGenres: function() {
+        for (let i = 1; i < 4; i++) {
+            let genr = '';
+            while (genr == '' || genr == null) {
+                genr = prompt(`Ваш любимый жанр под номером ${i}?`);
+            }
+            personalMovieDB.genres.push(genr);
+        }
+        personalMovieDB.genres.forEach((elem, index) => {
+            console.log(`Любимый жанр #${index+1} - это ${elem}`);   
+        });
+         
+        console.log(personalMovieDB);
+    },
+    toggleVisibleMyDB: function() {
+        personalMovieDB.privat = !personalMovieDB.privat;
     }
-}
-
-console.log(availableCurr(baseCurrencies.concat(additionalCurrencies), 'UAH'));
+};
