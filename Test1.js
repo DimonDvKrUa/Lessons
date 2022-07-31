2e8bb53c214bb6c4c3ed453896919bc1cb6f493e
Dimon Home <dimon@dv.kr.ua>
2022-07-31 17:35:17 +0300

    Упражнение по написанию кода 16: (*) Задача на рекурсию

--- Test1.js
+++ Test1.js
@@ -1,25 +1,24 @@
-var rows = prompt("How many rows for your multiplication table?");
-    var cols = prompt("How many columns for your multiplication table?");
-    if(rows == "" || rows == null)
-   		 rows = 10;
-    if(cols== "" || cols== null)
-   		 cols = 10;
-    createTable(rows, cols);
-    function createTable(rows, cols)
-    {
-      var j=1;
-      var output = "<table border='1' width='500' cellspacing='0'cellpadding='5'>";
-      for(i=1;i<=rows;i++)
-      {
-    	output = output + "<tr>";
-        while(j<=cols)
-        {
-  		  output = output + "<td>" + i*j + "</td>";
-   		  j = j+1;
-   		}
-   		 output = output + "</tr>";
-   		 j = 1;
+function factorial(x) {
+  if (x < 1) {
+    return 1;
+  } else if (!Number.isInteger(x)) {
+    return 'Ошибка';
+  } else {
+    if (x === 1) {
+      return x;
+    } else {
+      return x *= factorial(x - 1);
     }
-    output = output + "</table>";
-    document.write(output);
-    }
+  }
+}
+
+console.log(factorial(1)); // 1! = 1
+console.log(factorial(2)); // 2! = 2 * 1 = 2
+console.log(factorial(3)); // 3! = 3 * 2 * 1 = 6
+console.log(factorial(4)); // 4! = 4 * 3 * 2 * 1 = 24
+console.log(factorial(5)); // 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+console.log(factorial(-5)); 
+console.log(factorial(5.5));
+console.log(factorial('sdsfsd'));
+
